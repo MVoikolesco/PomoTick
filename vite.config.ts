849cd71f3316e182470603fdd288eff93ee51505
mvoikolesco <marcioavj.ti@outlook.com>
2024-06-07 02:29:58 -0300

    feat(vite): Added configurations to transform application to PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,35 @@ export default defineConfig({
     vue(),
     vueJsx(),
     VueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Pomotrick',
+        short_name: 'Pomotrick',
+        description: 'time management application with pomodoro',
+        theme_color: '#4DBA87',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-384x384.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+        ]
+      },
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+      }
+    })
   ],
   resolve: {
     alias: {
