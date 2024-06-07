09b79eac27df07040491427c8ae645589915986b
mvoikolesco <marcioavj.ti@outlook.com>
2024-06-07 02:28:41 -0300

    feat(pws): Added icons and images to PWA application

---
+++ src/registerServiceWorker.ts
@@ -0,0 +1,11 @@
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+        // Aqui você pode notificar o usuário que uma nova versão está disponível
+        console.log('Nova versão disponível. Atualize a página.');
+    },
+    onOfflineReady() {
+        console.log('O aplicativo está pronto para ser usado offline.');
+    },
+});
