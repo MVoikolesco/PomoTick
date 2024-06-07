8d60daa3f65a3dbf619fc1332dd7d0885bf6d607
mvoikolesco <marcioavj.ti@outlook.com>
2024-06-06 21:33:51 -0300

    feat(style): Added buttons on another card

--- pages/index.vue
+++ pages/index.vue
@@ -27,7 +27,7 @@ store.$subscribe(() => {
           <div class="counter-card">
             <CountdownTimer />
           </div>
-          <div>
+          <div class="action-buttons">
             <button @click="() => store.startTimer()">Start Timer</button>
             <button @click="() => store.pauseTimer()">Pause Timer</button>
             <button @click="() => store.resumeTimer()">Resume Timer</button>
@@ -43,5 +43,34 @@ store.$subscribe(() => {
 .home {
   width: 100%;
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  padding-top: 20px;
+}
+.home > .counter-card {
+  width: 80%;
+  padding: 26px 20px;
+  border-radius: 16px;
+  background-color: #292f49;
+
+  display: flex;
+  justify-content: center;
+
+  -webkit-box-shadow: -11px 17px 13px 8px rgba(11,21,41,1);
+  -moz-box-shadow: -11px 17px 13px 8px rgba(11,21,41,1);
+  box-shadow: -11px 17px 13px 8px rgba(11,21,41,1);
+}
+.home > .action-buttons {
+  width: 80%;
+  padding: 26px 20px;
+  border-radius: 16px;
+  background-color: #3c4267;
+
+  display: flex;
+
+  -webkit-box-shadow: -13px 10px 17px 8px rgba(35,43,68,1);
+  -moz-box-shadow: -13px 10px 17px 8px rgba(35,43,68,1);
+  box-shadow: -13px 10px 17px 8px rgba(35,43,68,1);
 }
 </style>
